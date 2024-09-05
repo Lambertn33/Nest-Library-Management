f69a5f593af061f191027b7aaf86f250073ebabf
lambertn33 <lambertgithub@gmail.com>
2024-09-05 20:55:29 +0200

    feat: admin create a book and view all books

--- src/books/books.controller.ts
+++ src/books/books.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { Roles } from 'src/auth/roles.decorator';
+import { UserRole } from 'src/auth/enum/role.enum';
+import { CreateBookDto } from './dto/create.dto';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+
+  @Get()
+  async findAll(
+    @Query('page') page: number = 1,
+    @Query('limit') limit: number = 10,
+  ) {
+    return this.booksService.findAll(page, limit);
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN)
+  async create(@Body() data: CreateBookDto) {
+    const checkBookTitle = await this.booksService._doesBookTitleExist(
+      data.title,
+    );
+    const checkBookNumber = await this.booksService._doesBookNumberExist(
+      data.book_no,
+    );
+
+    if (checkBookTitle) {
+      throw new BadRequestException(
+        'A book with such title exists in the Database',
+      );
+    }
+
+    if (checkBookNumber) {
+      throw new BadRequestException(
+        'A book with such book no exists in the Database',
+      );
+    }
+
+    return this.booksService.create(data);
+  }
+}
--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,7 +1,83 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class BooksService {
   constructor(private readonly databaseService: DatabaseService) {}
+
+  async findAll(page = 1, limit = 10) {
+    const skip = (page - 1) * limit;
+    const take = +limit;
+    const totalBooks = await this.databaseService.book.count();
+    const books = await this.databaseService.book.findMany({
+      skip,
+      take,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    return {
+      data: books,
+      meta: {
+        totalBooks,
+        currentPage: page,
+        totalPages: Math.ceil(totalBooks / limit),
+        limit,
+      },
+    };
+  }
+  async findOne(id: number) {}
+
+  async create(newBookInputs: Prisma.BookCreateInput) {
+    const { author, book_no, description, title } = newBookInputs;
+    const newBook = await this.databaseService.book.create({
+      data: {
+        author,
+        book_no,
+        description,
+        title,
+      },
+    });
+
+    return {
+      message: 'new book created successfully',
+      newBook,
+    };
+  }
+
+  async update(id: number, data: Prisma.BookUpdateInput) {}
+
+  async delete(id: number) {}
+
+  async _isBookBorrowed(id: number) {}
+
+  // check if book title has not been taken before
+  async _doesBookTitleExist(title: string) {
+    const book = await this.databaseService.book.findFirst({
+      where: {
+        title: {
+          equals: title,
+          mode: 'insensitive',
+        },
+      },
+    });
+
+    return !!book;
+  }
+
+  //check if book number has not been taken before
+  async _doesBookNumberExist(bookNo: string) {
+    const book = await this.databaseService.book.findFirst({
+      where: {
+        book_no: {
+          equals: bookNo,
+          mode: 'insensitive',
+        },
+      },
+    });
+
+    return !!book;
+  }
 }
