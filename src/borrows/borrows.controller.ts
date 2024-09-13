5a9dae48f840515908f64eb18a0f38ef4997aa8a
lambertn33 <lambertgithub@gmail.com>
2024-09-13 08:57:03 +0200

    feat: update readme

--- src/borrows/borrows.controller.ts
+++ src/borrows/borrows.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { BorrowsService } from './borrows.service';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
+import { RolesGuard } from 'src/auth/roles.guard';
+import { UserRole } from 'src/auth/enum/role.enum';
+import { Roles } from 'src/auth/roles.decorator';
+import { BorrowStatus } from './enum/borrow-status.enum';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(UserRole.ADMIN)
 @Controller('borrows')
-export class BorrowsController {}
+export class BorrowsController {
+  constructor(private readonly borrowsService: BorrowsService) {}
+
+  @Get()
+  async findAll(@Query('status') status?: BorrowStatus) {
+    return this.borrowsService.getAllBorrows(status);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.borrowsService.getSingleBorrow(id);
+  }
+
+  @Put(':/id/returnBook')
+  async confirmBookReturned(@Param('id', ParseIntPipe) id: number) {}
+}
